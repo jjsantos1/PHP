cc881674abde28ab24b6a6bb90fa04406f61cd9d
Jonathan Santos <jjsantos1@up.edu.ph>
2018-04-14 09:20:48 +0800

    php skillset 1

---
+++ my_solution.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Structure and Errors sample page - FIX ME!!!</title>
+</head>
+<body>
+<table style="border:1px solid black;">
+<tr>
+<td>
+<b>
+<?php
+echo ("This should be formatted nicely to be easy to read in this document. If you can not quickly and easily find out what the errors on this page are, then perhaps you should go back and re-examine the fifth movie in this chapter... And by the way, you if you can figure out all the errors on this page, then you are well on your way to becoming a
+good PHP programmer. Congratulations!");
+?>
+</b>
+</td>
+</tr>
+</table>
+</body>
+</html>
