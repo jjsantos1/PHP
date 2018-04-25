464e808339d554dbbb84d34acac9385c465c65bd
Jonathan Jontarciego Santos <34256647+jjsantos1@users.noreply.github.com>
2018-04-25 15:24:07 +0800

    calculator with user input

---
+++ calculator.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+function addNums($firstnum,$secondnum){
+	$result = $firstnum + $secondnum;
+	return $result;
+}
+function subNums($firstnum,$secondnum){
+	$result = $firstnum - $secondnum;
+	return $result;
+}
+function mulNums($firstnum,$secondnum){
+	$result = $firstnum * $secondnum;
+	return $result;
+}
+function divNums($firstnum,$secondnum){
+	$result = $firstnum / $secondnum;
+	return $result;
+}
+switch($_GET['math']){
+	case 'add':
+		echo addNums($_GET['first'],$_GET['second']);
+		break;
+	case 'subtract':
+		echo subNums($_GET['first'],$_GET['second']);
+		break;
+	case 'multiply':
+		echo mulNums($_GET['first'],$_GET['second']);
+		break;
+	case 'divide':
+		echo divNums($_GET['first'],$_GET['second']);
+		break;
+}
+?>
+</body>
+</html>
